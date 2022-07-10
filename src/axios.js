bacd077362a06fc42a6258758da71864697937e3
Swaraj Ballal <86348862+Swarajballal@users.noreply.github.com>
2022-07-10 19:22:13 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://us-central1-clone-44045.cloudfunctions.net/api'
-    // "http://localhost:5001/clone-44045/us-central1/api" 
+    baseURL: 'add you base url'
+    // "" 
 });
 
 export default instance;
